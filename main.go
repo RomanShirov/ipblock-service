84ea96f1e221e52a9d3e19b920fda0898e486795
RomanShirov <RomanShirov.dev@yandex.ru>
2022-11-29 15:26:22 +0300

    Edit main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
-		log.Fatalf("failed to listen %v", err)
+		log.Fatalf("Failed to listen %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
